574a8b52cedc93d01dc0bb5e59b9f131a16dffa8
Jade Jamig <ajamig016@gmail.com>
2021-05-29 21:00:00 +0800

    Initial Commit

---
+++ News-App-SwiftUI/News_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_App_SwiftUIApp.swift
+//  News-App-SwiftUI
+//
+//  Created by Anselm Jade Jamig on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct News_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
